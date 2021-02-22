8c128696b011dd26305f576391fdcfa7f2e113fd
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-02-22 10:39:17 +0100

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='cryptic_client',
+    packages=find_packages(),
+    version='0.9.0',
+    description='A Cryptic Client Library for Python',
+    author='Tert0',
+    license='MIT',
+    install_requires=[
+        'websocket-client',
+    ],
+    url='https://github.com/Tert0/cryptic-client',
+)
