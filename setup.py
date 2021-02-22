53908719a820823da5337a6881e40a2e424c515d
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-02-22 22:18:33 +0100

    Added README as Desc PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
+
+readme = file('README.md', 'r').read()
+
 setup(
     name='cryptic_sdk',
     packages=find_packages(),
     version='1.0.2',
     description='A Cryptic SDK for Python',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author='Tert0',
     license='MIT',
     install_requires=[
